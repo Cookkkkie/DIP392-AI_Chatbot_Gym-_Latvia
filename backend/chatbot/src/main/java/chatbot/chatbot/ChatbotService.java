92e7e5e5d9d8526321f8a3f7e2d574b04d9202a5
Oleksii Pecheniuk <aleksey.pecheniuk@icloud.com>
2025-04-12 13:41:07 +0300

    Added initial prompts

--- backend/chatbot/src/main/java/chatbot/chatbot/ChatbotService.java
+++ backend/chatbot/src/main/java/chatbot/chatbot/ChatbotService.java
@@ -1,34 +1,41 @@
 package chatbot.chatbot;
 
-import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.ChatResponse;
+import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.ollama.OllamaChatClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import reactor.core.publisher.Flux;
-
 
 @Service
 public class ChatbotService {
 
-    private final OllamaChatClient _chatModel;
-    
+    private final OllamaChatClient chatModel;
+    private final StringBuilder conversationHistory = new StringBuilder();
+
     @Autowired
-    public ChatbotService(OllamaChatClient chatModel) {
-        _chatModel = chatModel;
+    public ChatbotService(OllamaChatClient _chatModel) {
+        chatModel = _chatModel;
+        conversationHistory.append(InitialPrompts.chatBot);
+        conversationHistory.append(InitialPrompts.FAQ);
+        conversationHistory.append(InitialPrompts.GeneralTerms);
+        conversationHistory.append(InitialPrompts.internalRules);
+        conversationHistory.append(InitialPrompts.policies);
+
     }
 
     public String handleMessageRequest(String message) {
-        // here will be request to AI API to get response
-        System.out.println("Message received in Service: " + message);
-        // Prompt prompt = new Prompt(new UserMessage(message));
-        return _chatModel.call(message);
-    }  
+        conversationHistory.append("\nUser: ").append(message);
+        String response = chatModel.call(conversationHistory.toString());
+        conversationHistory.append("\nAI: ").append(response);
+        return response;
+    }
 
     public Flux<ChatResponse> generateStream(String message) {
-        Prompt prompt = new Prompt(new UserMessage(message));
-        return _chatModel.stream(prompt);
+        conversationHistory.append("\nUser: ").append(message);
+        Prompt prompt = new Prompt(new UserMessage(conversationHistory.toString()));
+        Flux<ChatResponse> stream = chatModel.stream(prompt);
+        return stream;
     }
 }
