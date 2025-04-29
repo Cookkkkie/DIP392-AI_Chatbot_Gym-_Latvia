ddb0e8aeceaaf58982e52b544c0c4b29a11f8eba
mnek1t <mednikita2004@gmail.com>
2025-04-30 00:36:05 +0300

    add redis

---
+++ backend/chatbot/src/main/java/chatbot/chatbot/RedisService.java
@@ -0,0 +1,26 @@
+package chatbot.chatbot;
+
+import java.time.Duration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RedisService {
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+    
+    public void saveHistory(String userId, String data) {
+        redisTemplate.opsForValue().append(userId, data);
+        redisTemplate.expire(userId, Duration.ofMinutes(30));
+    }
+
+    public String getHistory(String userId) {
+        return redisTemplate.opsForValue().get(userId);
+    }
+
+    public void deleteHistory(String userId) {
+        redisTemplate.delete(userId);
+    }
+}
