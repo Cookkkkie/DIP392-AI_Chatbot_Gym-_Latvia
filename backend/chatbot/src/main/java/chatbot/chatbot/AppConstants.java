b7d4cca44c47d1f0f0fa44badc091fdad88dc247
mnek1t <mednikita2004@gmail.com>
2025-05-24 02:01:56 +0300

    avoid magic numbers

--- backend/chatbot/src/main/java/chatbot/chatbot/AppConstants.java
+++ backend/chatbot/src/main/java/chatbot/chatbot/AppConstants.java
@@ -5,4 +5,5 @@ public class AppConstants {
     public static final String KNOWLEDGE_BASE_UPDATE_FAILURE_MESSAGE = "The knowledge base is not available at the moment. Suggest the customer contacts support at info@gymlatija.lv.";
     public static final String NO_FAQ_ERROR_MESSAGE = "Could not initialize FAQ";
     public static final String CHATBOT_DEFINITION = "You are a GymLatvija chatbot. Follow these rules:\\n";
+    public static final Integer CONVERSATION_HISTORY_EXPIRATION_TIME = 30; // in minutes
 }
